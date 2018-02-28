dfac712e23918bb755e9a72a9c98c37ed60c8065
anbudhanush5 <35687834+anbudhanush5@users.noreply.github.com>
2018-02-28 14:07:58 +0530

    Create 66.c

---
+++ 66.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+    int a,i,c=0;
+    printf("enter the number :");
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    {
+        if(a%i==0)
+        {
+            c++;
+        }
+    }
+    if(c==2)
+    {
+        printf("\n given number is prime ");
+
+    }
+    else
+    {
+        printf("given number is not prime");
+    }
+    getch();
+}
